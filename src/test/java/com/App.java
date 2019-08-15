4d1c1cbb4dd1f42f8790561c89ea1999bb883bbb
Ayushi Dhameliya <ayushi.dhameliya@gmail.com>
2019-08-14 17:24:02 -0700

    addition method

---
+++ src/test/java/com/App.java
@@ -0,0 +1,29 @@
+
+package com;
+
+
+
+public class App {
+	
+	public int addition() {
+		int a=1;
+		int b=2;
+		return (a+b);
+	}
+   
+	public static void main(String args[]) {
+		App a=new App();
+		
+		System.out.println("helloooo");
+		
+		
+		a.addition();
+		
+		
+		System.out.println("----");
+		
+		
+		
+	}
+}
+
